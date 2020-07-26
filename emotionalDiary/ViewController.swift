8aa687da658dfca6d673796c0bc5737a3f4f5b56
seoul2015 <seoul2015.11.14@gmail.com>
2020-07-26 23:29:44 +0900

    Initial Commit

---
+++ emotionalDiary/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  emotionalDiary
+//
+//  Created by PARK JEONGHUN on 2020/07/26.
+//  Copyright © 2020 CodeBear. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
