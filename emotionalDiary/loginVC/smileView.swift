019c967034500a6793bd885391fce5957b7bad23
tksqpek123 <greg00@hanmail.net>
2020-08-13 01:28:51 +0900

    loginVC에서 smile imageview, flagimageview 파일 작성 (cocoafile)

---
+++ emotionalDiary/loginVC/smileView.swift
@@ -0,0 +1,21 @@
+//
+//  smileView.swift
+//  emotionalDiary
+//
+//  Created by クォンスンギュン on 2020/08/13.
+//  Copyright © 2020 CodeBear. All rights reserved.
+//
+
+import UIKit
+
+class smileView: UIImageView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
